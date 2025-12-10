91a677d8ba0861c563ae3f8921e85ee6f1f3a3d7
zouyuoz <zouyuoz0720@gmail.com>
2025-12-10 09:02:44 +0000

    try to make wl1loss functionable

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from model import UNet
+from model import UNet, WeightedL1Loss
 import torch.utils.data as Data
 import numpy as np
 import argparse
@@ -7,7 +7,6 @@ import os
 from tqdm import tqdm
 import random
 from utils import *
-import auraloss
 
 """
     SVS-UNet Training Script with Validation
@@ -17,8 +16,6 @@ import auraloss
 # Determine device
 device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 print(f"Using device: {device}")
-mrstft_loss = auraloss.freq.MRSTFTLoss().to(device)
-alpha = 0.1
 
 # =========================================================================================
 # Dataset Definition
@@ -146,7 +143,6 @@ best_val_loss = 100.
 log_buffer = []
 scheduler = None
 start_epoch = 0
-optimizer = model.optimizer
 
 # 這是給您參考的載入寫法 (放在 train.py 初始化階段)
 if os.path.exists(args.load_path):
@@ -198,7 +194,7 @@ for ep in range(start_epoch, args.epoch):
     avg_train_loss = train_loss_sum / len(train_loader)
     log_buffer.append(f"{avg_train_loss}\n")
     
-    # --- Validation Phase (Every 20 epochs) ---
+    # --- Validation Phase (Every 10 epochs) ---
     if valid_loader and (ep + 1) % args.val_interval == 0:
         model.eval() # 切換到評估模式 (關閉 Dropout, BatchNorm 變動)
         val_loss_sum = 0
@@ -209,9 +205,11 @@ for ep in range(start_epoch, args.epoch):
             
             for mix, voc in val_loop:
                 mix, voc = mix.to(device), voc.to(device)
+                
+                # 手動計算 Loss (因為 model.backward 是訓練用的)
                 mask = model(mix)
-                loss_tensor = model.crit(mask * mix, voc) + model.crit((1 - mask) * mix, torch.clamp(mix - voc, min=0.0))
-                val_loss_sum += loss_tensor.item()
+                loss = WeightedL1Loss.forward(voc, mix, mask)
+                val_loss_sum += loss
         
         avg_val_loss = val_loss_sum / len(valid_loader)
         log_buffer.append(f"Val {avg_val_loss}\n")
@@ -261,11 +259,10 @@ print("Finish training!")
 python train.py \
     --train_folder unet_spectrograms/train \
     --valid_folder unet_spectrograms/valid \
-    --label L1_ft16 \
-    --batch_size 16 \
-    --epoch 500 \
-    --val_interval 20 \
-    --load_path CKPT/svs_1209_L1.ckpt
+    --label weightedL1 \
+    --batch_size 32 \
+    --epoch 400 \
+    --val_interval 10
     
 想法：
 
